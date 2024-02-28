c4428eba9cae94ba67f7ac7149637a3a317074b7
alejandrob <alejandrob@git.paypertic.com>
2024-02-28 20:58:34 -0300

    Agrego controlador y servicio de estadisticas

--- src/main/java/com/decrypto/tradehub/controllers/StatController.java
+++ src/main/java/com/decrypto/tradehub/controllers/StatController.java
@@ -1,4 +1,30 @@
 package com.decrypto.tradehub.controllers;
 
+import com.decrypto.tradehub.models.CountryStatsDTO;
+import com.decrypto.tradehub.models.Market;
+import com.decrypto.tradehub.services.StatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/stats")
 public class StatController {
+
+    private final StatService statService;
+
+    @Autowired
+    public StatController(StatService statService) {
+        this.statService = statService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<CountryStatsDTO>> getClientDistributionByMarketAndCountryStats() {
+        return ResponseEntity.ok(statService.calculateDistributionByCountryAndMarket());
+    }
 }
--- src/main/java/com/decrypto/tradehub/services/StatService.java
+++ src/main/java/com/decrypto/tradehub/services/StatService.java
@@ -1,4 +1,54 @@
 package com.decrypto.tradehub.services;
 
+import com.decrypto.tradehub.dao.ClientRepository;
+import com.decrypto.tradehub.entities.ClientEntity;
+import com.decrypto.tradehub.entities.MarketEntity;
+import com.decrypto.tradehub.models.CountryStatsDTO;
+import com.decrypto.tradehub.models.MarketStatsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
 public class StatService {
+
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public StatService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<CountryStatsDTO> calculateDistributionByCountryAndMarket() {
+        List<ClientEntity> clients = clientRepository.findAll();
+        long totalClients = clients.stream()
+                .mapToLong(client -> client.getMarkets().size())
+                .sum();
+        Map<String, Map<String, Long>> countryMarketCounts = new HashMap<>();
+
+        for (ClientEntity client : clients) {
+            for (MarketEntity market : client.getMarkets()) {
+                countryMarketCounts
+                        .computeIfAbsent(market.getCountry(), k -> new HashMap<>())
+                        .merge(market.getCode(), 1L, Long::sum);
+            }
+        }
+
+        return countryMarketCounts.entrySet().stream().map(countryEntry -> {
+            String country = countryEntry.getKey();
+            List<MarketStatsDTO> marketStats = countryEntry.getValue().entrySet().stream().map(marketEntry -> {
+                String marketName = marketEntry.getKey();
+                Long count = marketEntry.getValue();
+                String percentage = String.format("%.2f%%", (double) count / totalClients * 100);
+                return new MarketStatsDTO(marketName, percentage);
+            }).collect(Collectors.toList());
+
+            return new CountryStatsDTO(country, marketStats);
+        }).collect(Collectors.toList());
+    }
+
 }
